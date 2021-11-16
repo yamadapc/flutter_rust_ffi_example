11882607deaf7cab2cc20470a41da0d13eb89509
yamadapc <tacla.yamada@gmail.com>
2021-11-16 23:02:02 +1100

    Add some more demo code

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,59 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rust_ffi_example/rust_bindings.dart';
+import 'package:macos_ui/macos_ui.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return const AppLayout();
+  }
+}
+
+class AppLayout extends StatefulWidget {
+  const AppLayout({Key? key}) : super(key: key);
+
+  @override
+  State<AppLayout> createState() => _AppLayoutState();
+}
+
+class _AppLayoutState extends State<AppLayout> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MacosApp(
+        home: MacosWindow(
+      sidebar: Sidebar(
+          minWidth: 200,
+          topOffset: 0,
+          builder: (context, controller) {
+            return SidebarItems(
+                currentIndex: currentIndex,
+                onChanged: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                items: const [
+                  SidebarItem(label: Text("Main")),
+                  SidebarItem(label: Text("Development")),
+                  SidebarItem(label: Text("Settings")),
+                  SidebarItem(label: Text("About")),
+                ]);
+          }),
+      child: IndexedStack(
+          index: 0,
+          children: const [MyHomePage(title: 'Flutter Demo Home Page')]),
+    ));
   }
 }
 
@@ -69,47 +97,87 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return MacosScaffold(
+      titleBar: const TitleBar(title: Text("macOS")),
+      children: [
+        ContentArea(
+          builder: (buildContext, container) {
+            return Center(
+              // Center is a layout widget. It takes a single child and positions it
+              // in the middle of the parent.
+              child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text(
+                    'Click the button to reload rust library',
+                  ),
+                  ButtonsRow()
+                ],
+              ),
+            );
+          },
+        )
+      ],
     );
+  }
+}
+
+class ButtonsRow extends StatefulWidget {
+  const ButtonsRow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ButtonsRow> createState() => _ButtonsRowState();
+}
+
+class _ButtonsRowState extends State<ButtonsRow> {
+  DynamicLibrary? library;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      CupertinoButton(
+          child: const Text("Reload rust library"),
+          onPressed: () {
+            onClickReload();
+          }),
+      CupertinoButton(
+          child: const Text("Run rust function"),
+          onPressed: () {
+            onClickRun();
+          })
+    ], mainAxisAlignment: MainAxisAlignment.center);
+  }
+
+  void onClickReload() {
+    setState(() {
+      library = DynamicLibrary.open(
+          "/Users/yamadapc/projects/flutter_rust_ffi_example/target/debug/libflutter_rust_ffi_example.dylib");
+    });
+  }
+
+  void onClickRun() {
+    var lib = library;
+    if (lib == null) {
+      return;
+    }
+
+    var rustLib = RustBinding(lib);
+    rustLib.hello_world();
   }
 }
